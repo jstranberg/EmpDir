d9c683a18aeda02167bbd7dff9292bc88cdb9100
mistadarcy <masterhobo24@yahoo.com>
2015-04-15 07:37:07 +0900

    part 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 var app = {
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,8 +22,10 @@ var app = {
     },
 
     initialize: function() {
-		//can use three memory storage mediums. MemoryStore, LocalStorageStore, or WebSqlStore.
-        this.store = new WebSqlStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
